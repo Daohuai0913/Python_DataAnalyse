56fe600c38a77ec36734f4b574d976e5861280cd
huai <1411264142@qq.com>
2022-03-25 19:28:08 +0800

    changes

--- excesice.py
+++ excesice.py
@@ -1,6 +1,11 @@
-import pandas
 import pandas as pd
-import numpy as np
 
-data = pandas.read_excel("adult.data.xls")
-print(data.info)
+import pandas as pd
+
+df = pd.DataFrame({'name': ['张蔷', '伍剑中', '贾文静', '宋文燕'], 'weight': [100, 200, 100, 100],
+                   'gender': ['female', 'male', 'female', 'female']})
+print(df)
+
+print(df.rename(columns={'weight': '体重'}, copy=False))
+print("----------------------")
+print(df.rename(columns={'weight': '体重'}, copy=True))
